2fe6e63c000c5a8fa6f564a6bd13adfda36f3179
Kira <117554978+kiraware@users.noreply.github.com>
2025-06-12 12:08:09 +0700

    add News, NewsCategory, and NewsImage model and migration

---
+++ app/Models/News.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class News extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'news_category_id',
+        'published_at',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(NewsCategory::class);
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(NewsImage::class);
+    }
+}
