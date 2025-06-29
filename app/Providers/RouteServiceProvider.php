26fc1283423e2c2800c546872cde65362d6ec991
vexra <112993677+vexra@users.noreply.github.com>
2025-06-28 22:45:00 -0400

    style: run formatting code

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -4,8 +4,8 @@ namespace App\Providers;
 
 use App\Models\News;
 use App\Models\NewsCategory;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
